68e07183df13dc985a8bed108ac73409626030e0
zeroeuni <pkhj286@gmail.com>
2024-07-07 20:36:04 +0900

    Add : Router 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Main } from "../pages/main/ui/page/Page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
