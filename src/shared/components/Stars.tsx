0615f72e5e86af7f70890d4115ae93e7ad4ce872
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-10 08:53:20 +0900

    feat: star컴포넌트 퍼블리싱

---
+++ src/shared/components/Stars.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Star from "../../assets/Star";
+import BlueStar from "../../assets/BlueStar";
+
+interface StarsProps {
+  read?: boolean;
+  value?: number;
+  setting?: boolean;
+}
+
+const Stars = ({ read = false, value = 0, setting = false }: StarsProps) => {
+  const [currentStarsValue, setCurrentStartsValue] = useState(value);
+
+  const handleClick = (index: number) => {
+    if (setting) {
+      setCurrentStartsValue(index + 1);
+    }
+  };
+
+  return (
+    <div>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <span
+          key={index}
+          onClick={() => handleClick(index)}
+          style={{ cursor: setting ? "pointer" : "default" }}
+        >
+          {index < currentStarsValue ? <BlueStar /> : <Star />}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default Stars;
