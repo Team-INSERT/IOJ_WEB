bf0ea02aa500cc2e2f6a70d3c49a06aa82ca8da3
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-10 08:55:48 +0900

    feat: contesttitle 컴포넌트 퍼블리싱

---
+++ src/shared/components/InGame/ContestTitle/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import ContestTitle from "../../../../assets/ContestTitle.svg";
+import { NexonFont, theme } from "../../../../shared/style";
+
+export const BackgroundLayout = styled.div`
+  background-image: url(${ContestTitle});
+  width: 1089px;
+  height: 128px;
+  cursor: pointer;
+`;
+export const Layout = styled.div`
+  padding: 34px 0 17px 120px;
+`;
+export const Title = styled.div`
+  ${NexonFont.NexonBigTitle2}
+  color: ${theme.grey900};
+`;
+export const Date = styled.div`
+  ${NexonFont.NexonSmallText}
+  color: ${theme.grey600};
+`;
