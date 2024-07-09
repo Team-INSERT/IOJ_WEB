0ff6b8e54b9897a6f434bf7a02cb50fd1eeaf39d
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-10 08:56:36 +0900

    feat: usercompartment 컴포넌트 퍼블리싱

---
+++ src/shared/components/InGame/UserCompartment/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import InGameUserPartment from "../../../../assets/InGameUserPartment.svg";
+import InGameHoverUserPartment from "./../../../../assets/InGameHoverUserPartment.svg";
+import { NexonFont, flex } from "../../../style";
+
+export const Layout = styled.div`
+  background-image: url(${InGameUserPartment});
+  width: 148px;
+  height: 148px;
+  border-radius: 4px;
+  ${flex.COLUMN_CENTER}
+  cursor: pointer;
+  &:hover {
+    background-image: url(${InGameHoverUserPartment});
+  }
+`;
+export const UserName = styled.div`
+  ${NexonFont.NexonCaption}
+  padding-top: 7px;
+`;
