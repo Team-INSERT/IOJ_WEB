0a15ec71cbe270b5b25d0895a93c50587f68ce08
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-11 16:13:54 +0900

    feat: 게임 홈 페이지 퍼블리싱

---
+++ src/pages/room/ui/GameHome/index.tsx
@@ -0,0 +1,19 @@
+import { GameCard } from "@/shared/components";
+import * as S from "./style"
+
+const GameHome = () => {
+  return (
+    <S.Layout>
+      <S.Title>게임 시작하기</S.Title>
+      <S.SubTitle>원하는 모드와 인원을 선택해 주세요.</S.SubTitle>
+      <S.Line />
+      <S.ChooseGameMode>
+        <GameCard mode="베이직 모드" />
+        <GameCard mode="대회 모드" />
+        <GameCard mode="커스텀 모드" />
+      </S.ChooseGameMode>
+    </S.Layout>
+  )
+};
+
+export default GameHome;
