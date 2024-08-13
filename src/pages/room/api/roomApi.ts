4e829243edeb2145b0b981354fb93dda9a4ed67d
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-08-13 20:27:33 +0900

    chore : contestList url 수정

--- src/pages/room/api/roomApi.ts
+++ src/pages/room/api/roomApi.ts
@@ -1,7 +1,7 @@
 import { customAxios } from "@/shared/utils/customAxios";
 
 export const contestList = async () =>
-  (await customAxios.get(`/contest?page=0&size=3`)).data;
+  (await customAxios.get(`/contest`)).data;
 
 export const contestProblem = async (contestid: number) =>
   (await customAxios.get(`/contest/${contestid}`)).data;
