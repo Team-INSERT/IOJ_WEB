3172967ab80544e9dd0566aaface86c3a6451825
zeroeuni <pkhj286@gmail.com>
2024-08-14 12:55:30 +0900

    refactor: onClick 함수 하나로 합치기

--- src/pages/admin/ui/start/index.tsx
+++ src/pages/admin/ui/start/index.tsx
@@ -5,21 +5,34 @@ import * as S from "./style";
 export const Start = () => {
   const navigate = useNavigate();
 
-  const onContestClick = () => {
-    navigate("/admin/main");
+  const handleClick = (type: string) => {
+    if (type === "contest") {
+      navigate("/admin/main");
+    } else if (type === "question") {
+      navigate("/admin/Question");
+    }
   };
+
   return (
     <>
       <MainHeader />
       <S.StartLayout>
         <S.CreateContest>
-          <Button mode="big" color="blue" onClick={onContestClick}>
+          <Button
+            mode="big"
+            color="blue"
+            onClick={() => handleClick("contest")}
+          >
             대회 만들기
           </Button>
         </S.CreateContest>
         <S.Divider />
         <S.CreateQ>
-          <Button mode="big" color="blue">
+          <Button
+            mode="big"
+            color="blue"
+            onClick={() => handleClick("question")}
+          >
             문제 만들기
           </Button>
         </S.CreateQ>
