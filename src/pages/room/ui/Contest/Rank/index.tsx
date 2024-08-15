be2582d08c79aec0ec511f3a433c64aa636b3998
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-08-15 20:39:41 +0900

    feat : 대회 순위 리스트 api 연결

--- src/pages/room/ui/Contest/Rank/index.tsx
+++ src/pages/room/ui/Contest/Rank/index.tsx
@@ -1,73 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GameRankBlue from "@/assets/GameRankBlue";
 import GameRankGrey from "@/assets/GameRankGrey";
+import { gameRakingList } from "@/pages/room/api/roomApi";
 import * as S from "./style";
 
+interface ProblemStatuses {
+  status: string;
+  penalty: number | null;
+}
+
+interface Player {
+  nickname: string;
+  problemStatuses: ProblemStatuses[];
+}
+
 export const ContestRanking = () => {
+  const [playerDetail, setPlayerDetail] = useState<Player[]>([]);
   const title = "2024학년도 1학년 알고리즘 경진대회";
-  const questions = ["A", "B", "C", "D", "E", "F", "G", "H"];
-  const players = [
-    {
-      name: "강서호랑사또",
-      rank: "1",
-      questions: ["1", "wrong", "3", "2", "1", "2", "1", "3"],
-    },
-    {
-      name: "사상호랑사또",
-      rank: "2",
-      questions: ["1", "1", "3", "wrong", "1", "", "1", "3"],
-    },
-    {
-      name: "양산호랑사또",
-      rank: "3",
-      questions: ["1", "1", "3", "", "1", "2", "wrong", "3"],
-    },
-    {
-      name: "김해호랑사또",
-      rank: "4",
-      questions: ["1", "", "3", "wrong", "", "2", "1", "3"],
-    },
-    {
-      name: "대저호랑사또",
-      rank: "5",
-      questions: ["1", "wrong", "3", "2", "", "", "1", "3"],
-    },
-    {
-      name: "울산호랑사또",
-      rank: "6",
-      questions: ["1", "wrong", "3", "", "wrong", "2", "", "3"],
-    },
-    {
-      name: "기장호랑사또",
-      rank: "7",
-      questions: ["1", "wrong", "wrong", "2", "1", "", "1", "3"],
-    },
-    {
-      name: "양정호랑사또",
-      rank: "8",
-      questions: ["1", "1", "", "2", "wrong", "2", "wrong", "wrong"],
-    },
-    {
-      name: "만덕호랑사또",
-      rank: "9",
-      questions: ["1", "1", "3", "", "wrong", "wrong", "wrong", "3"],
-    },
-    {
-      name: "대연호랑사또",
-      rank: "10",
-      questions: ["1", "wrong", "wrong", "2", "1", "", "", "3"],
-    },
-    {
-      name: "광안호랑사또",
-      rank: "11",
-      questions: ["1", "wrong", "", "2", "1", "wrong", "wrong", "wrong"],
-    },
-    {
-      name: "서면호랑사또",
-      rank: "12",
-      questions: ["wrong", "wrong", "3", "", "wrong", "wrong", "", "3"],
-    },
-  ];
+  const questions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  useEffect(() => {
+    const playerList = async () => {
+      try {
+        const res = await gameRakingList();
+        setPlayerDetail(res);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    playerList();
+  }, []);
   return (
     <S.Layout>
       <S.BlueBg>
@@ -83,38 +45,43 @@ export const ContestRanking = () => {
             <S.PropertyText>순위</S.PropertyText>
             <S.PropertyText>이름</S.PropertyText>
             <S.QuestionsNames>
-              {questions.map((item) => (
-                <S.QuestionLayout questionSum={questions.length}>
-                  <S.QuestionName>{item}</S.QuestionName>
+              {playerDetail[0]?.problemStatuses.map((_, index) => (
+                <S.QuestionLayout
+                  questionSum={playerDetail[0].problemStatuses.length}
+                >
+                  <S.QuestionName>{questions[index]}</S.QuestionName>
                 </S.QuestionLayout>
               ))}
             </S.QuestionsNames>
           </S.Attribute>
           <S.BlueLine />
           <S.RankingLayout>
-            {players.map((player) => (
-              <S.UserRow key={player.rank}>
-                <S.Ranking rank={player.rank}>{player.rank}</S.Ranking>
-                <S.Name>{player.name}</S.Name>
-                <S.Questions>
-                  {player.questions.map((result, index) => {
-                    if (result === "") {
-                      return <S.NotSolved questionSum={questions.length} />;
-                    }
-                    if (result === "wrong") {
-                      return <S.RedQuestion questionSum={questions.length} />;
-                    }
-                    return (
-                      <S.Question questionSum={questions.length}>
-                        <S.QuestionSolveRank>
-                          {player.questions[index]}
-                        </S.QuestionSolveRank>
-                      </S.Question>
-                    );
-                  })}
-                </S.Questions>
-              </S.UserRow>
-            ))}
+            {playerDetail.map((player, index) => {
+              const rank = (index + 1).toString();
+              return (
+                <S.UserRow key={rank}>
+                  <S.Ranking rank={rank}>{rank}</S.Ranking>
+                  <S.Name>{player.nickname}</S.Name>
+                  <S.Questions>
+                    {player.problemStatuses.map((result) => {
+                      if (result.status === "unsolved") {
+                        return <S.NotSolved questionSum={questions.length} />;
+                      }
+                      if (result.status === "failed") {
+                        return <S.RedQuestion questionSum={questions.length} />;
+                      }
+                      return (
+                        <S.Question questionSum={questions.length}>
+                          <S.QuestionSolveRank>
+                            {result.penalty}
+                          </S.QuestionSolveRank>
+                        </S.Question>
+                      );
+                    })}
+                  </S.Questions>
+                </S.UserRow>
+              );
+            })}
           </S.RankingLayout>
         </S.Chart>
       </S.Content>
