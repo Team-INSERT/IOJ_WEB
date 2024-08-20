3f26cf9ccd15af6e08dc2e254e631ae9cb1810b4
seonghoon07 <chltjdgns1009@gmail.com>
2024-08-20 10:08:58 +0900

    chore: pull develop

--- src/pages/admin/ui/createC/index.tsx
+++ src/pages/admin/ui/createC/index.tsx
@@ -13,7 +13,7 @@ export interface postBodyProps {
   problems: number[];
 }
 
-export const Admin = () => {
+export const CreateContest = () => {
   const today = new Date();
   const formattedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`;
   const [startDay, setStartDay] = useState({ date: "", time: "" });
