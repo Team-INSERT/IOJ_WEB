b2c2672d6a97aa28b96ef36706269ee65a2cb5ef
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-08-20 14:46:25 +0900

    chore : ContestRanking 선언

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,13 @@ import { Main } from "@/pages/main";
 import { Game } from "@/pages/game";
 import { Login } from "@/pages/login";
 import { Setting } from "@/pages/setting";
-import { GameHome, ContestList, ContestQuestion } from "@/pages/room";
-import { Contest, Start, Question } from "@/pages/admin";
+import {
+  GameHome,
+  ContestList,
+  ContestQuestion,
+  ContestRanking,
+} from "@/pages/room";
+import { Start, Question } from "@/pages/admin";
 
 const App = () => {
   useAuthService();
@@ -18,13 +23,13 @@ const App = () => {
         <Route path="/setting" element={<Setting />} />
         <Route path="/game/contest" element={<ContestList />} />
         <Route path="/game/contest/:contestId" element={<ContestQuestion />} />
-        {/* <Route
+        <Route
           path="/game/contest/ranking/:contestId"
           element={<ContestRanking />}
-        /> */}
+        />
         <Route path="/game/contest/code" element={<Game />} />
         <Route path="/admin" element={<Start />} />
-        <Route path="/admin/Main" element={<Contest />} />
+        {/* <Route path="/admin/Main" element={<Contest />} /> */}
         <Route path="/admin/Question" element={<Question />} />
       </Routes>
     </Router>
