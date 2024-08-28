2b68b4c697a8caee7f0457180519d3e0d9ab0498
seonghoon07 <chltjdgns1009@gmail.com>
2024-08-28 20:35:50 +0900

    chore: 게임헤더 시간 수정

--- src/shared/components/GameHeader/index.tsx
+++ src/shared/components/GameHeader/index.tsx
@@ -45,12 +45,12 @@ const GameHeader = ({ problemsCount, problemIndex }: gameHeaderProps) => {
           clearInterval(interval);
           setRemainingTime("00 : 00 : 00");
         } else {
-          const hours = Math.floor((diff % (1000 * 3600 * 24)) / (1000 * 3600));
-          const minutes = Math.floor((diff % (1000 * 3600)) / (1000 * 60));
+          const hours = Math.floor(diff / (1000 * 60 * 60));
+          const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
           const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
           setRemainingTime(
-            `${String(hours).padStart(2, "0")} : ${String(minutes).padStart(2, "0")} : ${String(seconds).padStart(2, "0")}`,
+            `${String(hours).padStart(4, "0")} : ${String(minutes).padStart(2, "0")} : ${String(seconds).padStart(2, "0")}`,
           );
         }
       }, 1000);
