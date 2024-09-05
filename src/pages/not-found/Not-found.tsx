1c20701fe86f1a76f31a62586e8c3706b3218f5d
zeroeuni <pkhj286@gmail.com>
2024-09-05 19:05:33 +0900

    chore : 디자인 수정

--- src/pages/not-found/Not-found.tsx
+++ src/pages/not-found/Not-found.tsx
@@ -2,6 +2,7 @@ import * as S from "./style";
 
 export const NotFound = () => (
   <S.ErrorBox>
-    <S.ErrorText>404 | 페이지를 찾을 수 없습니다.</S.ErrorText>
+    <S.ErrorCode>404</S.ErrorCode>
+    <S.ErrorText>페이지를 찾을 수 없습니다</S.ErrorText>
   </S.ErrorBox>
 );
--- src/pages/not-found/style.ts
+++ src/pages/not-found/style.ts
@@ -3,8 +3,17 @@ import styled from "styled-components";
 
 export const ErrorBox = styled.div`
   height: 100vh;
-  ${flex.CENTER}
+  ${flex.COLUMN_CENTER}
 `;
+
+export const ErrorCode = styled.p`
+  ${flex.CENTER}
+  color: ${theme.black};
+  margin-bottom: 1%;
+  font-weight: 700;
+  font-family: ${Pretendard.BigTitle};
+`;
+
 export const ErrorText = styled.p`
   ${flex.CENTER}
   color: ${theme.black};
