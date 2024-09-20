2ba9ec9e86d933e3352b8babdb8bd757cf6bd4c2
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-09-20 21:14:29 +0900

    chore : 사용하지 않는 컴포넌트 제거

--- src/shared/components/boiler.ts
+++
@@ -1,4 +0,0 @@
-export const boilerplates = {
-  python: `# Python Boilerplate\nprint("Hello, World!")`,
-  java: `// Java Boilerplate\npublic class Main {\n  public static void main(String[] args) {\n    System.out.println("Hello, World!");\n  }\n}`,
-};
