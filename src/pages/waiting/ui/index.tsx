2abeece37588a297cd45aa213e5f065fa7a6840b
zeroeuni <pkhj286@gmail.com>
2024-09-21 12:16:11 +0900

    Add : 폴더 생성

---
+++ src/pages/waiting/ui/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import * as S from "./style";
+
+export const Waiting = () => <div></div>;
---
+++ src/pages/waiting/ui/style.ts
@@ -0,0 +1,2 @@
+import { flex, Pretendard, theme } from "@/shared/style";
+import styled from "styled-components";
