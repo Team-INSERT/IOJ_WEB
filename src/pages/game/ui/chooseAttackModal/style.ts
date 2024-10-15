5dc64b774c86c94d9d1858a209dcb1251f487bf0
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-10-15 20:34:35 +0900

    feat : 아이템 공격 선택 페이지

--- src/pages/game/ui/chooseAttackModal/style.ts
+++ src/pages/game/ui/chooseAttackModal/style.ts
@@ -6,9 +6,10 @@ export const Overlay = styled.div`
   position: fixed;
   width: 100vw;
   height: 100vh;
-  background-color: "rgba(0, 0, 0, 0.27)";
+  background-color: rgba(0, 0, 0, 0.27);
   ${flex.CENTER}
 `;
+
 export const Layout = styled.div`
   background-image: url(${chooseAttackBackground});
   width: 853px;
@@ -16,19 +17,41 @@ export const Layout = styled.div`
   ${flex.COLUMN_CENTER}
   padding: 32px 0 42px 0;
 `;
+
 export const Title = styled.span`
   ${NexonFont.NexonSmallTitle}
 `;
-export const MemberContainer = styled.div`
-  padding-top: 32px;
+
+export const MemberContainer = styled.div<{ itemCount: number }>`
+  display: grid;
+  grid-template-columns: repeat(
+    ${({ itemCount }) => Math.min(itemCount, 4)},
+    1fr
+  );
+  grid-auto-rows: auto;
+  gap: 20px;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+  ${({ itemCount }) =>
+    itemCount <= 4 &&
+    `
+    justify-items: center; /* 각 요소를 중앙으로 */
+    `}
 `;
+
 export const UserConpartment = styled.div`
-  width: 50%;
-  height: 50%;
+  background-color: aliceblue;
 `;
+
 export const CancelBtn = styled.button`
+  background-color: ${theme.white};
+  margin-top: 10px;
   padding: 13px 47.5px;
   border: 1px solid ${theme.warningRed};
   color: ${theme.warningRed};
   ${NexonFont.NexonText}
+  cursor: pointer;
+  &:hover {
+  }
 `;
