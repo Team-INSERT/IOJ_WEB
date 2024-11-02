d35d595e008e298bedfb678ffc09db6933314ba7
zeroeuni <pkhj286@gmail.com>
2024-11-02 22:07:28 +0900

    Add : 캐릭터 색상 오류 해결

--- src/shared/components/InGame/WaitingUser/index.tsx
+++ src/shared/components/InGame/WaitingUser/index.tsx
@@ -1,16 +1,32 @@
 import WaitingCharacter from "@/assets/WaitingCharacter";
 import * as S from "./style";
+import { theme } from "@/shared/style";
 
 interface Details {
   UserName: string;
   color: string;
 }
 
-const WaitingUser = ({ UserName, color }: Details) => (
-  <S.Layout>
-    {UserName && <WaitingCharacter color={color} />}
-    <S.UserName>{UserName}</S.UserName>
-  </S.Layout>
-);
+const colorMapping: { [key: string]: string } = {
+  BLUE: theme.BLUE, // "#6B71FF"
+  PINK: theme.PINK, // "#FF6AC3"
+  RED: theme.RED, // "#FF6B6B"
+  PURPLE: theme.PURPLE, // "#C446EE"
+  NEON: theme.NEON, // "#65EE83"
+  ORANGE: theme.ORANGE, // "#FF984D"
+  SKYBLUE: theme.SKYBLUE, // "#5CE2FF"
+  gray: theme.grey200,
+};
+
+const WaitingUser = ({ UserName, color = "gray" }: Details) => {
+  const characterColor = colorMapping[color] || colorMapping.gray;
+
+  return (
+    <S.Layout>
+      {UserName && <WaitingCharacter color={characterColor} />}
+      <S.UserName>{UserName}</S.UserName>
+    </S.Layout>
+  );
+};
 
 export default WaitingUser;
