aec0a73a409834c3577a032f15f13ab1831d52cc
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-11-03 23:47:01 +0900

    예시코드

---
+++ src/shared/components/Item/waterBalloon/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from "./style"
+
+const WaterBalloon = () => {
+  const a = 2;
+  return (
+    <S.Layout>
+      <S.Bar />
+      <S.Ballon>sdfasd</S.Ballon>
+      <S.Space />
+      <S.Point />
+    </S.Layout>
+  );
+};
+
+export default WaterBalloon;
---
+++ src/shared/components/Item/waterBalloon/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Layout = styled.div``;
+export const Bar = styled.div``;
+export const Ballon = styled.div``;
+export const Space = styled.div``;
+export const Point = styled.div``;
