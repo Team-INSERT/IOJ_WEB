bcc80e2ce5725b7cb74095d672ef1168a5464f7a
zeroeuni <pkhj286@gmail.com>
2024-11-06 11:53:35 +0900

    Add : 게임 시작 기능

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -75,6 +75,7 @@ const App = () => {
           <Route path="/game/find" element={<GameFind />} />
           <Route path="/game/waiting/:roomId" element={<Waiting />} />
           <Route path="/result" element={<Result />} />
+          <Route path="/game/:roomId" element={<Game />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
